79661bcf0c366d3187b219eb723a05e4a7a08610
John Wallace <John.Wallace@noaa.gov>
2020-12-04 19:44:04 -0800

    bug fixes

--- R/processData.R
+++ R/processData.R
@@ -1,6 +1,7 @@
 
-processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Grand_2019, Include.FishTime = FALSE, Restrict.6min = FALSE, Sites = NULL, Area = c("Orig121", "CCA", "ALL")[1], 
-                          reducedFormula = FALSE, propHookCutOffAggr = ifelse(Area == "ALL", 0.002, 0.003), propHookCutOffMirage = propHookCutOffAggr, propHookCutOffToro = 3 * propHookCutOffAggr) {
+processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Grand_2019, reducedFormula = FALSE, Include.FishTime = FALSE, Restrict.6min = FALSE, Sites = NULL, 
+                          Area = c("Orig121", "CCA", "ALL")[1], propHookCutOffAggr = if(Area == "ALL") 0.002 else 0.003, propHookCutOffMirage = propHookCutOffAggr, 
+                          propHookCutOffToro = 3 * propHookCutOffAggr) {
 
    #   -------- Import utility Functions --------
        sourceFunctionURL <- function(URL) {
@@ -17,7 +18,6 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
        sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/Table.R")
        sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/refactor.R")
        sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/recode.simple.R")
-       
  
    if(Restrict.6min) 
         Grand <- Grand[Grand$FishTime <= 360, ]
@@ -35,9 +35,10 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
    # printf(Table(Grand.TMP$CrewStaff); catf("\n\n")
 
    list.tmp <- list(Grand.TMP[,Y.Name])
-   names(list.tmp) <- paste("mean", Y.Name, sep=".")
+   mean.Y.Name <- paste("mean", Y.Name, sep=".")
+   names(list.tmp) <- mean.Y.Name
    catf("\n"); printf(r(cbind(aggregate(list.tmp, list(CrewStaff = Grand.TMP$CrewStaff, vessel = Grand.TMP$vessel), mean, na.rm = TRUE), 
-                      N = aggregate(list.tmp, list(Grand.TMP$CrewStaff, vessel = Grand.TMP$vessel), length)[,2])[,c("vessel", "CrewStaff", "N", "mean.NumBoc")]), 2); catf("\n\n")
+                      N = aggregate(list.tmp, list(Grand.TMP$CrewStaff, vessel = Grand.TMP$vessel), length)[,2])[,c("vessel", "CrewStaff", "N", mean.Y.Name)]), 2); catf("\n\n")
 
    Num.years.by.Site <- data.frame(site_number = dimnames(table(Grand.TMP$site_number, Grand.TMP$year))[[1]], 
 	           Numyears=apply(matrix(as.numeric(as.logical(table(Grand.TMP$site_number, Grand.TMP$year))), ncol=ncol(table(Grand.TMP$site_number, Grand.TMP$year))),1,sum))
@@ -45,18 +46,17 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
 
    Number <- aggregate(list(NUM = Grand.TMP[[Y.Name[1]]]), list(site_number=Grand.TMP$site_number), sum)
   
-  
+
   # Full formula includes SSTDrop.C, TideFlow2, TideHt, and DriftSpeedDrop.k - weight_kg and length_cm also added since they are now available
   if(reducedFormula) {
   
       if(Include.FishTime) {
           DATA <- Grand.TMP[Grand.TMP$site_number %in% Number[Number$NUM != 0, "site_number"] & Grand.TMP$Numyears >= 2, c(Y.Name, "year", "site_number", "vessel",
-              "drop_number", "hook_number", "angler_number", "sex", "CrewStaff", "depth_meters", "FishTime", "swell_height_m", "wave_height_m", "moon_phase_r", "moon_percent_fullness_r", "drop_time_proportion_of_solar_day",
-              "weight_kg", "length_cm")]
+              "drop_number", "hook_number", "angler_number", "sex", "CrewStaff", "depth_meters", "FishTime", "swell_height_m", "wave_height_m", "moon_phase_r", "moon_percent_fullness_r", "drop_time_proportion_of_solar_day")]
       } else {
       
           DATA <- Grand.TMP[Grand.TMP$site_number %in% Number[Number$NUM != 0, "site_number"] & Grand.TMP$Numyears >= 2, c(Y.Name, "year", "site_number", "vessel",
-              "drop_number", "hook_number", "angler_number", "sex", "CrewStaff", "depth_meters", "swell_height_m", "wave_height_m", "moon_phase_r", "moon_percent_fullness_r", "drop_time_proportion_of_solar_day", "weight_kg", "length_cm")]
+              "drop_number", "hook_number", "angler_number", "sex", "CrewStaff", "depth_meters", "swell_height_m", "wave_height_m", "moon_phase_r", "moon_percent_fullness_r", "drop_time_proportion_of_solar_day")]
       }
       
   } else {
@@ -64,12 +64,12 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
      if(Include.FishTime) {
           DATA <- Grand.TMP[Grand.TMP$site_number %in% Number[Number$NUM != 0, "site_number"] & Grand.TMP$Numyears >= 2, c(Y.Name, "year", "site_number", "vessel",
               "drop_number", "hook_number", "angler_number", "sex", "CrewStaff", "depth_meters", "FishTime", "swell_height_m", "wave_height_m", "moon_phase_r", "moon_percent_fullness_r", "drop_time_proportion_of_solar_day",
-              "SSTDrop.C", "TideFlow2", "TideHt", "DriftSpeedDrop.k", "weight_kg", "length_cm")]
+              "SSTDrop.C", "TideFlow2", "TideHt", "DriftSpeedDrop.k")]
       } else {
       
           DATA <- Grand.TMP[Grand.TMP$site_number %in% Number[Number$NUM != 0, "site_number"] & Grand.TMP$Numyears >= 2, c(Y.Name, "year", "site_number", "vessel",
               "drop_number", "hook_number", "angler_number", "sex", "CrewStaff", "depth_meters", "swell_height_m", "wave_height_m", "moon_phase_r", "moon_percent_fullness_r", "drop_time_proportion_of_solar_day", 
-              "SSTDrop.C", "TideFlow2", "TideHt", "DriftSpeedDrop.k", "weight_kg", "length_cm")]
+              "SSTDrop.C", "TideFlow2", "TideHt", "DriftSpeedDrop.k")]
      }
   }     
    
@@ -94,6 +94,7 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
    DATA$sex <- factor(as.character(DATA$sex))
    # DATA$TidePhase <- factor(as.character(DATA$TidePhase))
    
+   
    # Create Minor Angler Groups
    if(Area == "Orig121") 
       DATA <- DATA[as.numeric(as.character(DATA$site_number)) < 500, ] 
@@ -115,6 +116,7 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
    # Aggressor
    catf("\n\n--- Aggressor ---\n\nAll anglers:\n")
    printf(staffAggr <- sort(Table(DATA$CrewStaff[DATA$vessel %in% 'Aggressor'])))
+   catf(paste0("\nProportion of hooks fished cutoff value = ",  propHookCutOffAggr, ", Total number of hooks = ", sum(staffAggr), ", Prop times total = ", propHookCutOffAggr  * sum(staffAggr), "\n\n"))
    DATA$CrewStaff[DATA$CrewStaff %in% c(names(staffAggr[staffAggr < propHookCutOffAggr * sum(staffAggr)]), "UnknownAnglerUnknownAngler", "UnknownAggressor", "") & DATA$vessel %in% "Aggressor"] <- "MinorAnglerAggressor"
    printf(Table(CrewStaffOLD[DATA$CrewStaff %in% "MinorAnglerAggressor"], DATA$CrewStaff[DATA$CrewStaff %in% "MinorAnglerAggressor"])); catf("\n")
    catf("\nMinor Angler group added:\n")
@@ -123,6 +125,7 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
    # Mirage
    catf("\n\n--- Mirage ---\n\nAll anglers:\n")
    printf(staffMirage <- sort(Table(DATA$CrewStaff[DATA$vessel %in% 'Mirage'])))
+   catf(paste0("\nProportion of hooks fished cutoff value = ",  propHookCutOffMirage, ", Total number of hooks = ", sum(staffMirage), ", Prop times total = ", propHookCutOffMirage  * sum(staffMirage), "\n\n"))
    DATA$CrewStaff[DATA$CrewStaff %in% c(names(staffMirage[staffMirage < propHookCutOffMirage  * sum(staffMirage)]), "UnknownAnglerUnknownAngler", "UnknownMirage", "") & DATA$vessel %in% "Mirage"] <- "MinorAnglerMirage"
    printf(Table(CrewStaffOLD[DATA$CrewStaff %in% "MinorAnglerMirage"], DATA$CrewStaff[DATA$CrewStaff %in% "MinorAnglerMirage"])); catf("\n")
    catf("\nMinor Angler group added:\n")
@@ -131,6 +134,7 @@ processData <- function(Y.Name = "NumBoc", common_name = "Bocaccio", Grand = Gra
    # Toronado
    catf("\n\n--- Toronado ---\n\nAll anglers:\n")
    printf(staffToro <- sort(Table(DATA$CrewStaff[DATA$vessel %in% 'Toronado'])))
+   catf(paste0("\nProportion of hooks fished cutoff value = ",  propHookCutOffToro, ", Total number of hooks = ", sum(staffToro), ", Prop times total = ", propHookCutOffToro  * sum(staffToro), "\n\n"))
    DATA$CrewStaff[DATA$CrewStaff %in% c(names(staffToro[staffToro < propHookCutOffToro  * sum(staffToro)]), "UnknownAnglerUnknownAngler", "UnknownToronado", "") & DATA$vessel %in% "Toronado"] <- "MinorAnglerToronado"
    printf(Table(CrewStaffOLD[DATA$CrewStaff %in% "MinorAnglerToronado"], DATA$CrewStaff[DATA$CrewStaff %in% "MinorAnglerToronado"])); catf("\n")
    catf("\nMinor Angler group added:\n")

b50af42c501b483065844878739d2e7146fd6717
John Wallace <John.Wallace@noaa.gov>
2020-12-04 17:27:51 -0800

    Should not need my tool package attached

--- Example/Bocaccio Example.R
+++ Example/Bocaccio Example.R
@@ -9,7 +9,7 @@ require(MASS)
 require(gam)
 require(MCMCpack)
 require(Hmisc)
-require(JRWToolBox)
+
 
 # Take an intial look at the data
 Grand.2019 <- importData(grandPathCSV = "qryGrandUnifiedThru2019_For2021Assessments_DWarehouse version.csv")

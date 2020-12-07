f0f4d7bde50d9bc012ab952f3989c701a80209b0
John Wallace <John.Wallace@noaa.gov>
2020-12-07 01:51:41 -0800

    Update Bocaccio Example.R

--- Examples/Bocaccio Example.R
+++ Examples/Bocaccio Example.R
@@ -1,7 +1,8 @@
 
 
-# Early requires to check for packages
+remotes::install_github("John-R-Wallace-NOAA/HookandLineSurveyCatchAnalysis") 
 
+# Required packages listed here for help with installation (other packages may be installed by these packages).
 require(lattice)
 require(maptools)
 require(gtools) 
@@ -9,14 +10,17 @@ require(MASS)
 require(gam)
 require(MCMCpack)
 require(Hmisc)
+require(chron)
+require(RCurl)
+require(HookandLineSurveyCatchAnalysis) 
 
-
+   
 # Take an intial look at the data
 Grand.2019 <- importData(grandPathCSV = "qryGrandUnifiedThru2019_For2021Assessments_DWarehouse version.csv")
 Grand.2019[1:3, ]
 
 # Find a common name to run the analysis on
-table(Grand.2019$common_name, , useNA = "ifany")
+table(Grand.2019$common_name, useNA = "ifany")
 
      
 # Micro to test.  Unfortunately, 'reducedFormula' needs to TRUE for lack of variables in the current database.
@@ -24,27 +28,9 @@ Boc.2019.NFT.1k.121 <- HandL.stepAIC.MCMC(Y.Name = "NumBoc", common_name = "Boca
                              buffer = c(15, 45), tune = 0.14, mcmc = 1000, burnin = 10, thin = 2, verbose = 100, 
                              grandPathCSV = "W:/ALL_USR/JRW/Hook & Line Survey/2020/DATA/qryGrandUnifiedThru2019_For2021Assessments_DWarehouse version.csv")
   
-                                           
-# 105k MCMC - use GLM.FINAL.AIC from the micro run above
-Boc.2019.NFT.150k.121 <- HandL.stepAIC.MCMC(GLM.FINAL.AIC = Boc.Final.Glm, tune = 0.14, mcmc = 150000, burnin = 1000, thin = 150, verbose = 1000)
-  
-  
-  
-# --- Full runs with all args ---- 
-  
-# Micro to test
-Area = c('Orig121', 'CCA', 'ALL')[1] # Unlike in Linux, Win R can't take values from other args when explictly given in a call (it looks for the values in the calling frame)
-Boc.2019.NFT.1k.121 <- HandL.stepAIC.MCMC(Y.Name = "NumBoc", common_name = "Bocaccio", Area = c('Orig121', 'CCA', 'ALL')[1], reducedFormula = TRUE, Include.FishTime = FALSE, 
-                             Restrict.6min = FALSE, Sites = NULL, propHookCutOffAggr = if(Area == "ALL") 0.002 else 0.003, propHookCutOffMirage = propHookCutOffAggr, 
-                             propHookCutOffToro = 3 * propHookCutOffAggr, VermComplex = FALSE, Interaction = TRUE, buffer = c(15, 45), contrast = 'treatment', tune = 0.14, 
-                             mcmc = 1000, burnin = 10, thin = 2, verbose = 100, Stop.before.MCMC = FALSE, MAIN.STEP.AIC = NULL, STEP.AIC = NULL, GLM.FINAL.AIC = NULL, 
+# 150k MCMC iterations
+Boc.2019.NFT.150k.121 <- HandL.stepAIC.MCMC(Y.Name = "NumBoc", common_name = "Bocaccio", Area = c('Orig121', 'CCA', 'ALL')[1], reducedFormula = TRUE, Include.FishTime = FALSE,
+                             buffer = c(15, 45), tune = 0.14, mcmc = 150000, burnin = 1000, thin = 150, verbose = 1000, 
                              grandPathCSV = "W:/ALL_USR/JRW/Hook & Line Survey/2020/DATA/qryGrandUnifiedThru2019_For2021Assessments_DWarehouse version.csv")
-                             
-# 150k MCMC   
-Boc.2019.NFT.150k.121 <- HandL.stepAIC.MCMC(Y.Name = "NumBoc", common_name = "Bocaccio", Area = c('Orig121', 'CCA', 'ALL')[1], reducedFormula = TRUE, Include.FishTime = FALSE, 
-                             Restrict.6min = FALSE, Sites = NULL, propHookCutOffAggr = if(Area == "ALL") 0.002 else 0.003, propHookCutOffMirage = propHookCutOffAggr, 
-                             propHookCutOffToro = 3 * propHookCutOffAggr, VermComplex = FALSE, Interaction = TRUE, buffer = c(15, 45), contrast = 'treatment', tune = 0.14, 
-                             mcmc = 150000, burnin = 1000, thin = 150, verbose = 1000, Stop.before.MCMC = FALSE, MAIN.STEP.AIC = NULL, STEP.AIC = NULL, GLM.FINAL.AIC = NULL, 
-                             grandPathCSV = "W:/ALL_USR/JRW/Hook & Line Survey/2020/DATA/qryGrandUnifiedThru2019_For2021Assessments_DWarehouse version.csv")
-      
- 
+                                             
+

6f0556be469809e4355825aca1076cc09546ff94
John Wallace <John.Wallace@noaa.gov>
2022-06-17 15:16:01 -0700

    Updated sourceFunctionURL()

--- R/stepAIC.I.MCMC.R
+++ R/stepAIC.I.MCMC.R
@@ -8,12 +8,12 @@ stepAIC.I.MCMC <- function(Y.Name = 'NumBoc', DATA = DATA, Area = c("Orig121", "
   
   
     #   -------- Import utility Functions --------
-       sourceFunctionURL <- function(URL) {
+       sourceFunctionURL <-function(URL) {
           ' # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() '
           require(RCurl)
           File.ASCII <- tempfile()
           on.exit(file.remove(File.ASCII))
-          writeLines(paste(readLines(textConnection(RCurl::getURL(URL))), collapse = "\n"), File.ASCII)
+          writeLines(paste(readLines(jsonlite::fromJSON((URL))), collapse = "\n"), File.ASCII)
           source(File.ASCII, local = parent.env(environment()))
        }
        
